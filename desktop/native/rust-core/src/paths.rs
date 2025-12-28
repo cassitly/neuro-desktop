b066c8e3e22e3ff7584e229edb443f4ac14a456a
nakurity <238803201+nakurity@users.noreply.github.com>
2025-12-28 16:20:25 +0700

    Removed unused functions

--- desktop/native/rust-core/src/paths.rs
+++ desktop/native/rust-core/src/paths.rs
@@ -1,18 +1,9 @@
 use std::path::PathBuf;
 use std::env;
 
-pub fn bundled_python() -> PathBuf {
+pub fn get_python_packages_path() -> PathBuf {
     let exe = env::current_exe().unwrap();
     let root = exe.parent().unwrap();
 
-    root.join("python")
-        .join("binary")
-        .join("python.exe")
-}
-
-pub fn bundled_packages() -> PathBuf {
-    let exe = env::current_exe().unwrap();
-    let root = exe.parent().unwrap();
-
-    root.to_path_buf()
+    root.to_path_buf().join("python")
 }
