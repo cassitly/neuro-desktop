1e91d2ee689bcd8cf69587eb71bdabdece431838
cassitly <102461124+cassitly@users.noreply.github.com>
2025-12-30 14:52:04 +0700

    Revert main.go back
    We're not using this for the integration code anymore

--- desktop/native/go/main.go
+++ desktop/native/go/main.go
@@ -1,40 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"neuro/go/neuro-integration-sdk"
-)
+import "fmt"
 
 func main() {
-	client, err := neuro.NewClient("", "ws://localhost:8080/neuro")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer client.Close()
-
-	if err := client.Startup(); err != nil {
-		log.Fatal(err)
-	}
-
-	client.SendContext("Game started!", true)
-
-	// register actions your game supports
-	client.RegisterActions([]map[string]interface{}{
-		{"name": "move", "description": "Move someplace"},
-		{"name": "attack", "description": "Attack the enemy"},
-	})
-
-	for {
-		select {
-		case act := <-client.ActionChan:
-			fmt.Printf("Got action: %s\n", act.Name)
-
-			// validate + execute in your game here...
-			client.SendActionResult(act.ID, true, "ok")
-		case err := <-client.ErrChan:
-			log.Fatal("Neuro error:", err)
-		}
-	}
+	fmt.Println("Hello from Go!")
 }
