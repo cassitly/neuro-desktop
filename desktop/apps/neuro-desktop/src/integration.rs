85092c2e8f766e8449e5b419408029a2410f817c
cassitly <102461124+cassitly@users.noreply.github.com>
2025-12-30 13:56:26 +0700

    Completely refractor integration.rs

--- desktop/apps/neuro-desktop/src/integration.rs
+++ desktop/apps/neuro-desktop/src/integration.rs
@@ -1,114 +1,130 @@
+use std::{sync::Arc, time::Duration};
+
 use futures_util::{SinkExt, StreamExt};
+use neuro_sama::game::Api;
+use schemars::JsonSchema;
+use serde::Deserialize;
 use tokio::sync::mpsc;
-use tokio_tungstenite::{connect_async, tungstenite::Message};
-use url::Url;
 
-use neuro_sama::game::{
-    GameMessage,
-    RegisterActions,
-    ActionResult,
-};
+struct TestGame(mpsc::UnboundedSender<tungstenite::Message>);
 
-/// Message YOU send TO Neuro
-#[derive(Debug)]
-pub enum NeuroInput {
-    Context(String),
-    ActionResult {
-        action: String,
-        result: String,
-    },
+#[allow(unused)]
+#[derive(Debug, Deserialize, JsonSchema)]
+struct Action1 {
+    a: String,
+    b: u32,
+    c: u16,
+    d: bool,
 }
 
-/// Message Neuro sends TO YOU
-#[derive(Debug)]
-pub struct NeuroAction {
-    pub action: String,
-    pub data: serde_json::Value,
+#[allow(unused)]
+#[derive(Debug, Deserialize, JsonSchema)]
+struct Action2 {
+    a: u32,
+    b: bool,
 }
 
-/// Starts the Neuro integration in the background
-///
-/// Returns:
-/// - Sender: send context / results to Neuro
-/// - Receiver: receive actions from Neuro
-pub async fn start_integration(
-    game_name: &str,
-    ws_url: &str,
-) -> (
-    mpsc::Sender<NeuroInput>,
-    mpsc::Receiver<NeuroAction>,
-) {
-    let (to_neuro_tx, mut to_neuro_rx) = mpsc::channel::<NeuroInput>(32);
-    let (from_neuro_tx, from_neuro_rx) = mpsc::channel::<NeuroAction>(32);
+#[derive(Debug, Deserialize, JsonSchema)]
+struct Action3;
 
-    let game_name = game_name.to_string();
-    let ws_url = ws_url.to_string();
+#[derive(Debug, neuro_sama::derive::Actions)]
+enum Action {
+    /// Action 1 description
+    #[allow(unused)]
+    #[name = "action1"]
+    Action1(Action1),
+    /// Action 2 description
+    #[name = "action2"]
+    Action2(Action2),
+    /// Action 3 description
+    #[name = "action3"]
+    Action3(Action3),
+}
 
-    tokio::spawn(async move {
-        let url = Url::parse(&ws_url).expect("Invalid Neuro WS URL");
-        let (ws, _) = connect_async(url).await.expect("Neuro connect failed");
-
-        let (mut write, mut read) = ws.split();
-
-        // Register actions once
-        let register = RegisterActions {
-            game: game_name.clone(),
-            actions: vec![
-                ("move".into(), "Move somewhere".into()),
-                ("attack".into(), "Attack a target".into()),
-                ("wait".into(), "Do nothing".into()),
-            ],
-        };
-
-        write
-            .send(Message::Text(
-                serde_json::to_string(&GameMessage::RegisterActions(register)).unwrap(),
-            ))
-            .await
-            .unwrap();
-
-        loop {
-            tokio::select! {
-                // Incoming messages FROM Neuro
-                msg = read.next() => {
-                    let Some(Ok(Message::Text(text))) = msg else { continue };
-
-                    if let Ok(parsed) = serde_json::from_str::<GameMessage>(&text) {
-                        if let GameMessage::Action { action, data } = parsed {
-                            let _ = from_neuro_tx.send(NeuroAction {
-                                action,
-                                data,
-                            }).await;
-                        }
-                    }
-                }
-
-                // Messages FROM your game
-                Some(input) = to_neuro_rx.recv() => {
-                    let msg = match input {
-                        NeuroInput::Context(text) => {
-                            GameMessage::Context {
-                                game: game_name.clone(),
-                                context: text,
-                            }
-                        }
-                        NeuroInput::ActionResult { action, result } => {
-                            GameMessage::ActionResult(ActionResult {
-                                game: game_name.clone(),
-                                action,
-                                result,
-                            })
-                        }
-                    };
-
-                    write
-                        .send(Message::Text(serde_json::to_string(&msg).unwrap()))
-                        .await
-                        .unwrap();
+impl neuro_sama::game::Game for TestGame {
+    const NAME: &'static str = "Test Game";
+    type Actions<'a> = Action;
+    fn send_command(&self, message: tungstenite::Message) {
+        let _ = self.0.send(message);
+    }
+    fn reregister_actions(&self) {
+        // your game could have some complicated logic here i guess
+        self.register_actions::<Action>().unwrap();
+    }
+    fn handle_action<'a>(
+        &self,
+        action: Self::Actions<'a>,
+    ) -> Result<
+        Option<impl 'static + Into<std::borrow::Cow<'static, str>>>,
+        Option<impl 'static + Into<std::borrow::Cow<'static, str>>>,
+    > {
+        match action {
+            Action::Action3(_) => Err(Some("try again")),
+            Action::Action1(_) => Ok(None),
+            Action::Action2(act) => {
+                if act.b {
+                    Ok(Some("ok"))
+                } else {
+                    Err(Some("err"))
                 }
             }
         }
-    });
+    }
+}
 
-    (to_neuro_tx, from_neuro_rx)
+#[tokio::main(flavor = "current_thread")]
+async fn start_integration() {
+    let (game2ws_tx, mut game2ws_rx) = mpsc::unbounded_channel();
+    let game = Arc::new(TestGame(game2ws_tx));
+    game.initialize().unwrap();
+    let game1 = game.clone();
+    tokio::spawn(async move {
+        loop {
+            tokio::time::sleep(Duration::from_secs(20)).await;
+            game1
+                .force_actions::<Action>("do your thing".into())
+                .with_state("some state idk")
+                .send()
+                .unwrap();
+        }
+    });
+    let mut ws =
+        tokio_tungstenite::connect_async(if let Ok(url) = std::env::var("NEURO_SDK_WS_URL") {
+            url
+        } else {
+            "ws://127.0.0.1:8000".to_owned()
+        })
+        .await
+        .unwrap()
+        .0;
+    loop {
+        tokio::select! {
+            msg = game2ws_rx.recv() => {
+                println!("game2ws {msg:?}");
+                let Some(msg) = msg else {
+                    break;
+                };
+                if ws.send(msg).await.is_err() {
+                    println!("websocket send failed");
+                    break;
+                }
+            }
+            msg = ws.next() => {
+                println!("ws2game {msg:?}");
+                let Some(msg) = msg else {
+                    break;
+                };
+                let Ok(msg) = msg else {
+                    continue;
+                };
+                if let Err(err) = game.handle_message(msg) {
+                    // this could happen because we don't know what this message means (e.g. added
+                    // in a new version of the API)
+                    println!("notify_message failed: {err}");
+                    continue;
+                }
+            }
+
+        }
+    }
 }
